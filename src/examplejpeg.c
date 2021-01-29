6d41240c979b8a0ad8db1c6aa0b7b8fac4baf05b
josura <josuradev@hotmail.com>
2021-01-29 12:22:28 +0100

    example of zoom of png image with GPU and stb_image lib

--- src/examplejpeg.c
+++ src/examplejpeg.c
@@ -2,9 +2,9 @@
 #include<string.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include"stb/stb_image.h" 
+#include"../stb/stb_image.h" 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include"stb/stb_image_write.h" 
+#include"../stb/stb_image_write.h" 
 
 
 #define CL_TARGET_OPENCL_VERSION 120
